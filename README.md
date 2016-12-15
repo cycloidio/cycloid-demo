19f50185e4827be0735c4b233a8765c371c72b05
talset <florian.lambert@enovance.com>
2016-12-15 17:09:59 +0100

    temporary: use html cycloid website version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cycloid-demo
+
 html page used in the demo pipeline
+
+
+New color
+```
+sed -i 's/0f9797/38407c/g' imgs/main.css
+```
+
+Orig color
+```
+sed -i 's/38407c/0f9797/g' imgs/main.css
+```
+
+To generate an error please add this pattern in the index
+```
+ERROR
+```
---
+++ make_change.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#If orig color
+echo "Make change"
+if [ $(grep 0f9797 ./imgs/main.css -c) = 0 ]; then
+  comment="revert to 0f9797"
+  sed -i 's/38407c/0f9797/g' imgs/main.css
+else
+  comment="change to 38407c"
+  sed -i 's/0f9797/38407c/g' imgs/main.css
+fi
+
+echo $comment
+
+echo "git config"
+git config --global user.name "Cycloid"
+git config --global user.email contact@cycloid.io
+
+echo "git commit/push"
+git add imgs/main.css
+git commit -m "make a change : $comment"
+
+# Increment version number
+source version
+# We can increment major/minor/patch (0.0.0)
+bumpversion --current-version $version  patch version --commit --tag
+source version
+
+echo "git push tag"
+git push origin master
+git push origin v$version
