d2f4eb91f99f2593010ab1279b5c39440216c0c0
talset <florian.lambert@enovance.com>
2017-02-27 14:46:21 +0100

    add git config

--- make_change.sh
+++ make_change.sh
@@ -9,7 +9,12 @@ else
   comment="change to 38407c"
   sed -i 's/0f9797/38407c/g' imgs/main.css
 fi
+
 echo $comment
+
+echo "git config"
+git config --global user.name "Cycloid"
+git config --global user.email contact@cycloid.io
 
 echo "git commit"
 git add imgs/main.css
